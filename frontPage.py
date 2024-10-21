c6b651de3614bc7c4d93f3f1e08271ce2e4973d8
mykyta07 <mykytanyk@gmail.com>
2024-10-22 00:05:38 +0300

    added delete functional

--- frontPage.py
+++ frontPage.py
@@ -1,5 +1,5 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QMenu, QWidget,  QTableWidgetItem, QDialog, QPushButton, QHBoxLayout
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtWidgets import QMainWindow, QMenu, QWidget,  QTableWidgetItem, QDialog, QPushButton, QHBoxLayout, QMessageBox
 from PySide6.QtGui import QAction, QIcon
 from busesDialog import Ui_busesDialog
 from ui_index import Ui_MainWindow
@@ -53,7 +53,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def load_buses_data(self):
         conn = sqlite3.connect('bus_management.db')
         cursor = conn.cursor()
-        cursor.execute("SELECT model, number_plate, mileage, service_due_to FROM Buses")
+        cursor.execute("SELECT id, model, number_plate, mileage, service_due_to FROM Buses")
         rows = cursor.fetchall()
         conn.close()
 
@@ -66,19 +66,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.tableWidget.setRowCount(len(rows))
         for row_idx, row_data in enumerate(rows):
-            for col_idx, col_data in enumerate(row_data):
+            for col_idx, col_data in enumerate(row_data[1:]):
                 
                 
                 self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(str(col_data)))
 
                 double_button_widget = DoubleButtonWidget(row_idx, row_data)
+                double_button_widget.edit_clicked.connect(self.load_buses_data)
+                double_button_widget.delete_clicked.connect(self.load_buses_data)
                 self.tableWidget.setCellWidget(row_idx, 4, double_button_widget)
 
 
 class BusesDialog(QDialog, Ui_busesDialog):
     def __init__(self, parent=None):
         super(BusesDialog, self).__init__(parent)
-        self.setupUi(self)  # This is how you set up the UI
+        self.setupUi(self)  
 
         self.pushButton.clicked.connect(self.submit_data)
         self.pushButton_2.clicked.connect(self.reject)
@@ -106,6 +108,9 @@ class DoubleButtonWidget(QWidget):
         self.row_idx = row_idx
         self.row_data = row_data
         self.init_ui()
+    
+    edit_clicked = Signal(int)  # Signal for edit button clicked
+    delete_clicked = Signal(int)  # Signal for delete button clicked
 
     def init_ui(self):
         self.edit_button = QPushButton()
@@ -136,16 +141,14 @@ class DoubleButtonWidget(QWidget):
                 background-color: #cc0000;
             }
         """)
-        self.edit_button.setMinimumSize(50, 20)  # Minimum size
-        self.edit_button.setMaximumSize(50, 20)  # Maximum size
-        self.delete_button.setMinimumSize(50, 20)  # Minimum size
+        self.edit_button.setMinimumSize(50, 20)  
+        self.edit_button.setMaximumSize(50, 20)  
+        self.delete_button.setMinimumSize(50, 20)  
         self.delete_button.setMaximumSize(50, 20)
 
-        # Connect buttons to their respective slot methods
         self.edit_button.clicked.connect(self.edit_row)
         self.delete_button.clicked.connect(self.delete_row)
 
-        # Layout to arrange buttons horizontally
         layout = QHBoxLayout()
         layout.addWidget(self.edit_button)
         layout.addWidget(self.delete_button)
@@ -155,4 +158,13 @@ class DoubleButtonWidget(QWidget):
         print("Editing row", self.row_idx)
 
     def delete_row(self):
-        print("Deleting row", self.row_idx)
+        reply = QMessageBox.question(self, 'Delete Record',
+                                     "Are you sure you want to delete this record?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            conn = sqlite3.connect('bus_management.db')
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Buses WHERE id = ?", (self.row_data[0],))
+            conn.commit()
+            conn.close() 
+            self.delete_clicked.emit(self.row_idx)  # Emit signal for delete button clicked
